933186600f5b1c00b2c4759aad2f5782cc58ab3d
10r <69302186+TormentedProgram@users.noreply.github.com>
2026-06-18 00:01:34 +1000

    Adjust zoom clamp range from 1.0-10.0 to 0.1-10.0

--- src/main.rs
+++ src/main.rs
@@ -185,7 +185,7 @@ fn main() {
         if delta_scale.abs() > 0.5 {
             let p0 = scale_pivot / rl_camera.zoom;
             rl_camera.zoom = (rl_camera.zoom as f64 + delta_scale * rl.get_frame_time() as f64)
-                .clamp(1.0, 10.) as f32;
+                .clamp(0.1, 10.) as f32;
             let p1 = scale_pivot / rl_camera.zoom;
             rl_camera.target += p0 - p1;
             delta_scale -= delta_scale * rl.get_frame_time() as f64 * 4.0
